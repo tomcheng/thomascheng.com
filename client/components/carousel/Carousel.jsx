0e47f5564dac9b3901093d98a7e0c9fc2416df5d
Thomas Cheng <thomas@freshbooks.com>
2015-07-04 00:40:25 -0400

    Don't pan if vertical gesture is detected. Don't skip panes.

--- client/components/carousel/Carousel.jsx
+++ client/components/carousel/Carousel.jsx
@@ -9,10 +9,12 @@ export default React.createClass({
 
   getInitialState() {
     return {
-      width: 0,
+      dragDistance: 0,
+      isDragging: false,
+      isDraggingHorizontal: false,
       pane: 0,
-      drag: 0,
-      isDragging: false
+      swipeActived: false,
+      width: 0
     };
   },
 
@@ -42,10 +44,12 @@ export default React.createClass({
     });
   },
 
-  _handleSwipe(event) {
+  _handleSwipe(e) {
     const {pane} = this.state;
 
-    switch(event.direction) {
+    this.setState({ swipeActived: true });
+
+    switch(e.direction) {
       case 2: // left
         this._setPane(pane + 1)
         break;
@@ -55,34 +59,64 @@ export default React.createClass({
     }
   },
 
-  _handlePan(event) {
-    if (event.eventType === 4) { // release
-      if (Math.abs(event.deltaX) > this.state.width / 2) {
-        if (event.deltaX < 0) {
-          this._setPane(this.state.pane + 1);
-        } else {
-          this._setPane(this.state.pane - 1);
+  _handlePan(evt) {
+    const {isDragging, isDraggingHorizontal, swipeActived} = this.state;
+    const {eventType, deltaX, direction} = evt;
+
+    if (eventType === 4) { // release
+      if (swipeActived) {
+        this.setState({
+          swipeActived: false,
+          dragDistance: 0,
+          isDragging: false
+        });
+      } else {
+        if (Math.abs(deltaX) > this.state.width / 2) {
+          if (deltaX < 0) {
+            this._setPane(this.state.pane + 1);
+          } else {
+            this._setPane(this.state.pane - 1);
+          }
         }
+        this.setState({
+          dragDistance: 0,
+          isDragging: false
+        });
       }
+      return;
+    }
+
+    if (isDragging && isDraggingHorizontal) {
       this.setState({
-        drag: 0,
-        isDragging: false
-      });
-    } else {
-      this.setState({
-        drag: event.deltaX,
+        dragDistance: deltaX,
         isDragging: true
       });
+      return;
     }
+
+    if (!isDragging) {
+      if (direction === 8 || direction === 16) {
+        this.setState({
+          isDragging: true,
+          isDraggingHorizontal: false
+        })
+      } else {
+        this.setState({
+          isDragging: true,
+          isDraggingHorizontal: true
+        });
+      }
+    }
+
   },
 
   render() {
     const {images} = this.props;
-    const {width, pane, drag, isDragging} = this.state;
+    const {width, pane, dragDistance, isDragging} = this.state;
     const imageCount = images.length;
     const paneOffset = -100 * pane / imageCount;
-    let dragOffset = 100 * drag / width / imageCount;
-    if (pane === 0 && drag > 0 || pane === imageCount - 1 && drag < 0) {
+    let dragOffset = 100 * dragDistance / width / imageCount;
+    if (pane === 0 && dragDistance > 0 || pane === imageCount - 1 && dragDistance < 0) {
       dragOffset *= 0.4;
     }
     const containerStyle = {
@@ -96,7 +130,7 @@ export default React.createClass({
 
     return (
       <div>
-        <HammerComponent onSwipe={this._handleSwipe} onPan={this._handlePan}>
+        <HammerComponent vertical onSwipe={this._handleSwipe} onPan={this._handlePan}>
           <div className="carousel" style={{ width: width }}>
             <ul className={carouselListClasses} style={containerStyle}>
               {images.map((image, index) => (
