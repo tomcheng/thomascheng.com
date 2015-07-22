c281f27585eaee928f296e09a66ae55b7929a477
Thomas Cheng <thomas@freshbooks.com>
2015-07-22 00:08:07 -0400

    Move piece data to Portfolio component.

--- client/components/portfolio/Portfolio.jsx
+++ client/components/portfolio/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React from 'react/addons';
 
 import Piece from './Piece.jsx';
-import pieces from 'data/portfolio.jsx';
 
 export default React.createClass({
   render() {
@@ -14,3 +13,54 @@ export default React.createClass({
     );
   }
 });
+
+const pieces = [
+  {
+    slug: "guide",
+    title: "Typography Guide",
+    description: "A booklet of serious lessons I've learned in typography and design.",
+    imageCount: 6
+  },
+  {
+    slug: "futura",
+    title: "Type Specimen",
+    description: "Type specimen for Futura Book. *Warning* Inside contains sexually explicit typography intended for mature audiences.",
+    imageCount: 6
+  },
+  {
+    slug: "foodland",
+    title: "Foodland Ontario",
+    description: "Foodland Ontario mailer targeted to senior citizens.",
+    imageCount: 4
+  },
+  {
+    slug: "food-economy",
+    title: "Food Economy",
+    description: "Magazine layout for a target audience of affluent, educated professionals.",
+    imageCount: 2
+  },
+  {
+    slug: "two-fonts",
+    title: "Rebellion!",
+    description: "A project to combine three fonts to make a new one, and using it to express an appropriate phrase.",
+    imageCount: 1
+  },
+  {
+    slug: "diabetes",
+    title: "Fast Food Typography",
+    description: "Yummy lettering.",
+    imageCount: 1
+  },
+  {
+    slug: "heart",
+    title: "Hey Man",
+    description: "<3",
+    imageCount: 1
+  },
+  {
+    slug: "influential",
+    title: "Influential Typographers",
+    description: "Design and production of a book on influential type designers.",
+    imageCount: 3
+  }
+];
