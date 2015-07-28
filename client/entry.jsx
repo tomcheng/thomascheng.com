c492b80273f6334a7f1f400d4cf6392aad4e48a3
Thomas Cheng <thomas@freshbooks.com>
2015-07-27 23:36:10 -0400

    Move route handlers to their own folder. Add a resume route handler.

--- client/entry.jsx
+++ client/entry.jsx
@@ -3,12 +3,14 @@ import "styles/main.sass";
 import React from "react";
 import Router from "react-router";
 
-import Home from "components/home/Home.jsx";
 import Header from "components/header/Header.jsx";
-import Portfolio from "components/academic-work/AcademicWork.jsx";
-import FreshBooks from "components/freshbooks/FreshBooks.jsx";
-import PhotocopiedFaces from "components/photocopied-faces/PhotocopiedFaces.jsx";
 import Navigation from "components/navigation/Navigation.jsx";
+
+import Home from "components/route-handlers/Home.jsx";
+import Portfolio from "components/route-handlers/AcademicWork.jsx";
+import FreshBooks from "components/route-handlers/FreshBooks.jsx";
+import PhotocopiedFaces from "components/route-handlers/PhotocopiedFaces.jsx";
+import Resume from "components/route-handlers/Resume.jsx";
 
 const {DefaultRoute, Link, Route, RouteHandler} = Router;
 
@@ -34,6 +36,10 @@ const App = React.createClass({
           {
             path: "/photocopied-faces",
             title: "Photocopied Faces",
+          },
+          {
+            path: "/resume",
+            title: "Resume",
           }
         ]} />
     );
@@ -46,6 +52,7 @@ const routes = (
     <Route name="academic-work" path="/academic-work" handler={Portfolio} />
     <Route name="freshbooks" path="/freshbooks" handler={FreshBooks} />
     <Route name="photocopied-faces" path="/photocopied-faces" handler={PhotocopiedFaces} />
+    <Route name="resume" path="/resume" handler={Resume} />
   </Route>
 );
 
