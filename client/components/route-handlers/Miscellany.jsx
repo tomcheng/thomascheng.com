4f9e846ca06958f035a60c40f871f9e60ebaba77
Thomas Cheng <thomas@freshbooks.com>
2015-08-18 22:55:34 -0400

    Add Processing images to Miscellany.

--- client/components/route-handlers/Miscellany.jsx
+++ client/components/route-handlers/Miscellany.jsx
@@ -9,11 +9,17 @@ export default React.createClass({
         <div className="push-bottom">
           <Carousel
             description="These images were produced by rolling and smearing my face along the glass as the photocopied was scanning."
-            images={faces}
-            originalHeight={411}
-            originalWidth={411}
+            images={photocopiedFaces}
             slug="photocopied-faces"
             title="Photocopied Faces"
+          />
+        </div>
+        <div className="push-bottom">
+          <Carousel
+            description="Some experiments with Processing."
+            images={processingFaces}
+            slug="processing-faces"
+            title="Processing Faces"
           />
         </div>
       </div>
@@ -21,12 +27,19 @@ export default React.createClass({
   }
 });
 
-const faces = [
+const photocopiedFaces = [
+  require("images/photocopied-faces/pcface4.jpg"),
   require("images/photocopied-faces/pcface1.jpg"),
   require("images/photocopied-faces/pcface2.jpg"),
   require("images/photocopied-faces/pcface3.jpg"),
-  require("images/photocopied-faces/pcface4.jpg"),
-  require("images/photocopied-faces/pcface5.jpg"),
-  require("images/photocopied-faces/pcface6.jpg"),
   require("images/photocopied-faces/pcface7.jpg")
+];
+
+const processingFaces = [
+  require("images/processing-faces/diagonal-stripe-man.jpg"),
+  require("images/processing-faces/squareman.png"),
+  require("images/processing-faces/discowoman.jpg"),
+  require("images/processing-faces/stripeman.jpg"),
+  require("images/processing-faces/circlewoman.jpg"),
+  require("images/processing-faces/radiating.jpg")
 ];
