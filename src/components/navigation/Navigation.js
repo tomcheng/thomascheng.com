0e0d73fd8d3fbd86d56b6f9176613157fa6e111a
Thomas Cheng <thomascheng81@gmail.com>
2017-01-20 21:14:10 -0500

    Start moving styles to component

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import styled from "styled-components";
 import classNames from "classnames";
 
 const LINKS = [
@@ -9,37 +10,99 @@ const LINKS = [
   { title: "Contact", path: "/contact", hiddenOnMobile: true },
 ];
 
+const Header = styled.div`
+  background-color: rgba(255, 255, 255, .96);
+  border-bottom: 1px solid #e0e0e0;
+  top: 0;
+  z-index: 1000;
+  left: 0;
+  position: fixed;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    height: 45px;
+  }
+  @media (min-width: 768px) {
+    height: 70px;
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Container = styled.div`
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  @media (min-width: 768px) {
+    width: 750px
+  }
+  @media (min-width: 992px) {
+    width: 970px
+  }
+`;
+
+const MobileLink = styled(Link)`
+  @media (min-width: 768px) {
+    display: none
+  }
+`;
+
+const HomeIcon = styled.i`
+  font-size: 24px;
+  color: #333;
+`;
+
+const DesktopLink = styled(Link)`
+  @media (max-width: 767px) {
+    display: none;
+  }
+`;
+
+const Name = styled.div`
+  color: #333;
+  font-size: 14px;
+  font-weight: 900;
+  text-transform: uppercase;
+  letter-spacing: 0.5px;
+  font-family: Raleway, sans-serif;
+`;
+
+const Position = styled.div`
+  color: #666;
+  font-style: italic;
+  font-size: 13px;
+`;
+
 const Navigation = ({ location }) => {
   const isHome = location.pathname === "/";
   const isResume = location.pathname === "/resume";
 
   return (
-    <div className={classNames("header", {
-      "header--resume": isResume,
-    })}>
-      <div className="container relative-box">
-        <div className="pull-left">
-          <Link className="header__logo-container" to="/">
-            <i className="fa fa-home header__logo" />
-          </Link>
-          <div className="header__name-and-position">
-            <Link to="/">
-              <div className="header__name">
-                Thomas Cheng
-              </div>
-              {isResume ? (
-                <div className="header__position">
-                  thomascheng81@gmail.com | 647-772-3277 | 502-160 Baldwin St, Toronto, ON, M5T 3K7
-                </div>
-              ) : (
-                <div className="header__position">
-                  Front-End Developer &amp; Designer
-                </div>
-              )}
-
-            </Link>
-          </div>
-        </div>
+    <Header>
+      <Container style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: "100%",
+      }}>
+        <MobileLink to="/">
+          <HomeIcon className="fa fa-home" />
+        </MobileLink>
+        <DesktopLink to="/">
+          <Name>
+            Thomas Cheng
+          </Name>
+          {isResume ? (
+            <Position>
+              thomascheng81@gmail.com | 647-772-3277 | 502-160 Baldwin St, Toronto, ON, M5T 3K7
+            </Position>
+          ) : (
+            <Position>
+              Front-End Developer &amp; Designer
+            </Position>
+          )}
+        </DesktopLink>
         {!isResume ? (
           <ul className={classNames("navigation", {"navigation--home": isHome})}>
             {LINKS.map((link, i) => (
@@ -58,8 +121,8 @@ const Navigation = ({ location }) => {
             ))}
           </ul>
         ) : null}
-      </div>
-    </div>
+      </Container>
+    </Header>
   );
 };
 
