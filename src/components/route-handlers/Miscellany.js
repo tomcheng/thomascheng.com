5ebcb011485de85cbdccc7fa178915bc41ac6672
Thomas Cheng <thomascheng81@gmail.com>
2017-08-20 22:32:28 -0400

    Add bunch of quotes

--- src/components/route-handlers/Miscellany.js
+++ src/components/route-handlers/Miscellany.js
@@ -6,6 +6,14 @@ const Miscellany = () =>
     pieces={[
       {
         type: "link",
+        title: "Quotes",
+        url: "https://bunchofquotes.com",
+        width: 704,
+        height: 468,
+        image: require("../../images/miscellaneous/bunch-of-quotes.png")
+      },
+      {
+        type: "link",
         title: "Voronoia",
         url: "https://voronoia.com",
         width: 704,
